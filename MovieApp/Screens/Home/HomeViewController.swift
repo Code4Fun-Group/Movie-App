14138d3f5c279dea675a10c4f39560957497ed82
minh5896 <83936069+minh5896@users.noreply.github.com>
2021-10-22 15:43:48 +0700

    update

--- MovieApp/Screens/Home/HomeViewController.swift
+++ MovieApp/Screens/Home/HomeViewController.swift
@@ -158,3 +158,29 @@ extension HomeViewController: CellDelegate {
 }
 extension HomeViewController: UITableViewDelegate {
 }
+// MARK: - Enum
+private enum CaseCell: String, CaseIterable {
+	case home, preview, continueCell, list, europe, romance, action
+}
+
+private extension CaseCell {
+	func title() -> String {
+		switch self {
+		case .home:
+			return "Home"
+		case .preview:
+			return "Preview"
+		case .continueCell:
+			return "Continue"
+		case .list:
+			return "List"
+		case .europe:
+			return "Europe"
+		case .romance:
+			return "Romance"
+		case .action:
+			return "Action"
+
+		}
+	}
+}
