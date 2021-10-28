1e82441962d32268f1c29701ab0a1e8e84e17f27
Dong <dong2810123@gmail.com>
2021-10-28 16:34:17 +0700

    update search

--- MovieApp/Screens/Details/DetailsViewController.swift
+++ MovieApp/Screens/Details/DetailsViewController.swift
@@ -7,32 +7,14 @@
 
 import UIKit
 
-private enum Constants {
-	static let detailTBVC = "DetailTableViewCell"
-	static let moreTBVC = "MoreTableViewCell"
-}
-
 protocol IDetailViewController: AnyObject {
 	func showMovies()
 	func showError(_ errorMessage: String)
 }
 
 class DetailsViewController: BaseViewController {
-	// MARK: - IBOutlets
-	@IBOutlet weak var tableView: UITableView!
-
-	// MARK: - Life cycles
 	override func viewDidLoad() {
 	super.viewDidLoad()
-		setupUI()
-//		tableView.delegate = self
-//		tableView.dataSource = self
 	}
 }
 
-private extension DetailsViewController {
-	func setupUI() {
-//		tableView.register(UINib(nibName: Constants.detailTBVC, bundle: Bundle.main), forCellReuseIdentifier: Constants.detailTBVC)
-//		tableView.register(UINib(nibName: Constants.moreTBVC, bundle: Bundle.main), forCellReuseIdentifier: Constants.moreTBVC)
-	}
-}
--- MovieApp/Screens/Details/Views/DetailCell/More/DetailViewModel.swift
+++ MovieApp/Screens/Details/Views/DetailCell/More/DetailViewModel.swift
@@ -7,35 +7,3 @@
 
 import Foundation
 
-protocol IDetailViewModel {
-	func getMovies()
-
-}
-class DetailViewModel {
-
-	weak var viewController: IDetailViewController?
-	var detailViewModels = [IMovieViewModel]()
-
-	init(viewController: IDetailViewController) {
-		self.viewController = viewController
-	}
-}
-
-// MARK: - IDetailViewModel
-//extension DetailViewModel: IDetailViewModel {
-//	func getMovies() {
-//		DependencyResolver.shared.movieAPIService.getMovies { [weak self] result in
-//			guard let self = self else { return }
-//			switch result {
-//			case .success(let movies):
-//				self.detailViewModels = movies.compactMap({
-//					MovieViewModel(item: $0)
-//
-//				})
-//				self.viewController?.showMovies()
-//			case .failure(let error):
-//				self.viewController?.showError(error.localizedDescription)
-//			}
-//		}
-//	}
-//}
